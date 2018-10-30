ef22a5f35b116410a32f98de075fada8ecd33769
GabrielaSO <gaitsotoor@ittepic.edu.mx>
2018-10-30 00:25:54 -0700

    define modulos para 'tabs' y 'new'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { NewPage } from '../pages/new/new';
+import { TabsPage} from '../pages/tabs/tabs';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    NewPage,
+    TabsPage,
   ],
   imports: [
     BrowserModule,
@@ -19,7 +23,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    NewPage,
+    TabsPage,
   ],
   providers: [
     StatusBar,
